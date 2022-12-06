b6f0c7b416e937292bcc8b847f0295b5de927924
hadi dasti <hadidasti98@gmail.com>
2022-12-07 00:18:46 +0330

    start lookup

--- controller/worker/contract/startContractController.js
+++ controller/worker/contract/startContractController.js
@@ -1,7 +1,7 @@
 const StartContract = require('../../../model/worker/contract/StartContractWorker')
 
 
-
+// create startWorker
 exports.createStartContract = async(req,res)=>{
     try{
         const{
@@ -37,6 +37,68 @@ exports.createStartContract = async(req,res)=>{
             msg :'CREATE_START_WORK'
         })
 
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({
+            success : false,
+            msg :'Internal Server Error'
+        })
+    }
+}
+
+// get document startWorker with aggregation pip line and lookup
+exports.getAllStartContractWorker = async(req,res)=>{
+    try{
+        const startContractWorker = await StartContract.aggregate([
+            {
+                $lookup:{
+                    from:'workers',
+                    localField :'workerId',
+                    foreignField :'_id',
+                    as :'worker'
+                }
+            },
+            {
+                $unwind:'$worker'
+            },
+            {
+                $lookup:{
+                    from :'contractstructures',
+                    localField :'contractId',
+                    foreignField: '_id',
+                    as :'contractStructure'
+                }
+            },
+            {
+                $unwind:'$contractStructure'
+            },
+            {
+                $lookup:{
+                    from :'acceptcontracts',
+                    localField: 'acceptContractId',
+                    foreignField: '_id',
+                    as :'acceptContract'
+                }
+            },
+            {
+                $unwind:'$acceptContract'
+            }
+        ])
+
+        if(!startContractWorker){
+            return res.status(404).json({
+                success : false,
+                msg :'ERROR-NOT-FOUND'
+            })
+        }
+
+        return res.status(200).json({
+            success : true,
+            data : {
+                startContractWorker
+            },
+            msg :'SUCCESSFULLY LOOKUP'
+        })
     }catch(err){
         console.log(err)
         return res.status(500).json({
